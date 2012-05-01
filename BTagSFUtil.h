65885481f0240baee08ba060f8c8687b25fe3a4e
pandolf <>
2012-05-01 08:55:50 +0000

    major change: implemented functions are data members, added init functions, implemented modifyBTagsWithSF_fast

--- BTagSFUtil.h
+++ BTagSFUtil.h
@@ -45,7 +45,28 @@ class BTagSFUtil {
  public:
 
   BTagSFUtil( int seed=0 );
-  //BTagSFUtil( const std::string& btagAlgo, const std::string& wp, int seed=0 );
+  BTagSFUtil( const std::string& btagAlgo, int seed=0 );
+
+  void init( const std::string& wp );
+  void InitSFLight( const std::string& wp );
+  void InitMistag( const std::string& wp );
+  void InitSFb( const std::string& wp );
+  void checkInit( const std::string& wp );
+
+  float GetSFLight( float pt, float eta, const std::string& wp, const std::string& meanminmax="mean" );
+  float GetMistag( float pt, float eta, const std::string& wp, const std::string& meanminmax="mean" );
+  float GetSFb( float pt, float eta, const std::string& wp );
+
+  TF1* GetFunctionEtaBins( float eta, const std::vector<float>& etaBins, const std::vector<TF1*>& functions ) const;
+
+
+/***********************************/
+/* UP TO DATE FUNCTIONS*/
+/***********************************/
+  void SF(const std::string& btagAlgo, const std::string& wp, float pt = 60., float eta = 1.6);
+  void modifyBTagsWithSF_fast( bool& isBTagged_loose, bool& isBTagged_medium, float jetpt, float jeteta, int pdgIdPart, const std::string& meanMinMax="mean" );
+  void modifyBTagsWithSF(const std::string& btagAlgo , bool& isBTagged_loose, bool& isBTagged_medium, float jetpt, float jeteta, int pdgIdPart, float sysSF=1.0);
+
 
 /***********************************/
 /* FUNCTIONS NOT UP TO DATE */
@@ -54,13 +75,6 @@ class BTagSFUtil {
   void set_fileLoose( TFile* file );
   void modifyBTagsWithSF( bool& isBTagged_loose, bool& isBTagged_medium, int pdgIdPart, float Btageff_SF_l = 0.95,float Btageff_SF_m = 0.94, float Btagmistag_SF_l = 1.0,float Btagmistag_SF_m = 1.0, float Btagmistag_eff_l = 1.0, float  Btagmistag_eff_m = 1.0);
 
-/***********************************/
-/* UP TO DATE FUNCTIONS*/
-/***********************************/
-  void SF(const std::string& btagAlgo, const std::string& wp, float pt = 60., float eta = 1.6);
-  void modifyBTagsWithSF(const std::string& btagAlgo , bool& isBTagged_loose, bool& isBTagged_medium, float jetpt, float jeteta, int pdgIdPart, float sysSF=1.0);
-
-
   // deprecated:
   BTagScaleFactor  getSF( const std::string& fileName, float jetpt, float jeteta );
   BTagScaleFactor  getSF( const std::string& fileName, float jetpt );
@@ -68,8 +82,42 @@ class BTagSFUtil {
 
  private:
 
+  std::string btagAlgo_;
+
+  bool init_loose_;
+  bool init_medium_;
+  bool init_tight_;
+
   float SFb_;
   float SFlight_, Mistag_;
+
+  std::vector<float> etaBins_SFLight_loose_;
+  std::vector<TF1*> sfLightFunct_loose_;
+  std::vector<TF1*> sfLightFunct_loose_min_;
+  std::vector<TF1*> sfLightFunct_loose_max_;
+
+  std::vector<float> etaBins_SFLight_medium_;
+  std::vector<TF1*> sfLightFunct_medium_;
+  std::vector<TF1*> sfLightFunct_medium_min_;
+  std::vector<TF1*> sfLightFunct_medium_max_;
+
+  std::vector<float> etaBins_SFLight_tight_;
+  std::vector<TF1*> sfLightFunct_tight_;
+  std::vector<TF1*> sfLightFunct_tight_min_;
+  std::vector<TF1*> sfLightFunct_tight_max_;
+
+  std::vector<float> etaBins_mistag_loose_;
+  std::vector<TF1*> mistagFunct_loose_;
+
+  std::vector<float> etaBins_mistag_medium_;
+  std::vector<TF1*> mistagFunct_medium_;
+
+  std::vector<float> etaBins_mistag_tight_;
+  std::vector<TF1*> mistagFunct_tight_;
+
+  TF1* SFbFunct_loose_;
+  TF1* SFbFunct_medium_;
+
 
   TRandom3* rand_;
   std::string sfFileName_;
